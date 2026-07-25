102e83c0a5bf787e22dfabcdf8f294e613af35e6
polisettychakrasaradhi-ai <polisettychakrasaradhi@gmail.com>
2026-07-25 20:35:21 +0530

    Create 5b.java

---
+++ 5b.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class StringCharacterCount {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        int vowels = 0;
+        int consonants = 0;
+        int digits = 0;
+        int specialChars = 0;
+
+        str = str.toLowerCase();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            } else if (ch >= '0' && ch <= '9') {
+                digits++;
+            } else if (ch != ' ') {
+                specialChars++;
+            }
+        }
+
+        System.out.println("\nResults:");
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+        System.out.println("Digits: " + digits);
+        System.out.println("Special Characters: " + specialChars);
+
+        sc.close();
+    }
+}
